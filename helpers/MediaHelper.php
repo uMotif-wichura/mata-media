4097f45d74a342402100dc11ed658a90208532fe
Michal Fiedorowicz <michal@qi-interactive.com>
2015-03-16 16:54:50 +0000

    Updates

---
+++ helpers/MediaHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace mata\media\helpers;
+
+use matacms\widgets\videourl\helpers\VideoUrlHelper;
+use yii\helpers\Json;
+
+class MediaHelper
+{
+	public static function getType($mimeType)
+	{
+		return substr($mimeType,0, strrpos($mimeType,'/'));
+	}
+
+	public static function getPreview($media)
+	{
+		$type = self::getType($media->MimeType);
+		switch($type) {
+			case 'image':
+				return '<img src="' . $media->URI . '" draggable="false">';
+				break;
+			case 'video':
+				return '<img src="' . Json::decode($media->Extra, false)->thumbnailUrl . '" draggable="false">';
+				break;
+			default:
+			break;
+		}	
+	}
+}
