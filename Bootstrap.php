76179fbc56886b5182ef760aea4c44de5be4a15a
Marcin Wiatr <marcin@qi-interactive.com>
2015-05-28 17:19:06 +0100

    updates
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- Bootstrap.php
+++ Bootstrap.php
@@ -14,6 +14,7 @@ use yii\base\Model;
 use yii\db\BaseActiveRecord;
 use mata\media\models\Media;
 use matacms\controllers\module\Controller;
+use mata\arhistory\models\Revision;
 
 class Bootstrap implements BootstrapInterface 
 {
@@ -25,6 +26,10 @@ class Bootstrap implements BootstrapInterface
 			return;
 		
 		Event::on(Controller::class, Controller::EVENT_MODEL_CREATED, function(\matacms\base\MessageEvent $event) {
+			$this->updateDocumentIds($event->getMessage());
+		});
+
+		Event::on(Controller::class, Controller::EVENT_MODEL_UPDATED, function(\matacms\base\MessageEvent $event) {
 			$this->updateDocumentIds($event->getMessage());
 		});
 
@@ -42,29 +47,87 @@ class Bootstrap implements BootstrapInterface
 		});
 	}
 
+	private function manageMediaDeletions($model) {
+
+		$existingMedia = Media::find()->where(["LIKE", "For", $model->documentId->getId()])->all();
+
+		foreach ($existingMedia as $media) {
+
+			$documentId = $media->For;
+
+			if (!in_array($documentId, $_POST["Media"])) {
+				// Media has been removed from the core model, update version
+				$revision = $media->getLatestRevision();
+				$revisionAttributes = unserialize($revision->Attributes);
+
+				foreach ($revisionAttributes as $k => $v) {
+					if ($k == "Id")
+						continue;
+					
+					$revisionAttributes[$k] = null;
+				}
+
+				// $revision->Attributes = serialize($revisionAttributes);
+				$newRevision = new Revision();#
+				$newRevision->DocumentId = $revision->DocumentId;
+				$newRevision->Revision = $revision->Revision + 1;
+				$newRevision->Attributes = serialize($revisionAttributes);
+
+				if ($newRevision->save() == false)
+					throw new \yii\web\HttpException(500, $media->getTopError());
+			}
+		}
+
+	}
+
 	private function updateDocumentIds($model) 
 	{
-		$tmpDocumentIds = \Yii::$app->request->post('Media');
-		if(!empty($tmpDocumentIds)) {
-			foreach ($tmpDocumentIds as $tmpDocumentId) {
-				$this->updateDocumentId($model, $tmpDocumentId);
+		$mediaFromRequest = \Yii::$app->request->post('Media', []);
+		foreach ($mediaFromRequest as $sourceWidgetId => $mediaPayload) {
+
+			if (array_key_exists("delete", $mediaPayload)) {
+				$documentId = $mediaPayload["delete"];
+				$media = Media::find()->where(["For" => $documentId])->one();
+				$media->setAttributes([]);
+				$media->For = $documentId;
+
+				if ($media->save() == false)
+					throw new \yii\web\HttpException(500, $media->getTopError());
+			} else {
+				$this->updateDocumentId($model, $mediaPayload);
 			}
 		}
 	}
 
-	private function updateDocumentId($model, $tmpDocumentId)
+	private function updateDocumentId($model, $mediaPayload)
 	{
-		$attributePos = strpos($tmpDocumentId, "::");
+
+		$documentId = $mediaPayload["DocumentId"];
+		$attributeName = null;
+
+		$attributePos = strpos($documentId, "::");
+
 		if($attributePos)
-			$attribute = substr(substr($tmpDocumentId, $attributePos), 2);
+			$attributeName = substr(substr($documentId, $attributePos), 2);
 
-		$media = Media::find()->where(["DocumentId" => $tmpDocumentId])->one();
+		if ($attributeName == null)
+			throw new \yii\web\HttpException(500, "attributeName cannot be null");
 
-		if($tmpDocumentId && !empty($media) && $model->getDocumentId()) {
-			$media->DocumentId = $model->getDocumentId($attribute);
-			if ($media->save() == false)
-				throw new \yii\web\HttpException(500, $media->getTopError());
-		}
+
+		$documentId = $model->getDocumentId($attributeName)->getId();
+
+		$media = Media::find()->where(["For" => $documentId])->one();
+
+		if ($media == null)
+			$media = new Media();
+
+		$media->attributes = $mediaPayload;
+
+		$media->For = $documentId;
+
+		if ($media->save() == false)
+			throw new \yii\web\HttpException(500, $media->getTopError());
+
 	}
 
 	private function canRun($app) {
--- controllers/S3Controller.php
+++ controllers/S3Controller.php
@@ -1,10 +1,4 @@
-<?php
- 
-/**
- * @link http://www.matacms.com/
- * @copyright Copyright (c) 2015 Qi Interactive Limited
- * @license http://www.matacms.com/license/
- */
+<?php 
 
 namespace mata\media\controllers;
 
@@ -12,6 +6,8 @@ use Aws\S3\S3Client;
 use mata\keyvalue\models\KeyValue;
 use mata\media\models\Media;
 use yii\helpers\Json;
+use mata\base\DocumentId;
+use mata\arhistory\models\Revision;
 
 class S3Controller extends \mata\web\Controller {
 
@@ -27,14 +23,15 @@ class S3Controller extends \mata\web\Controller {
 
 		$this->setResponseContentType("application/json");
 
-		$s3Key = KeyValue::findValue(self::S3_KEY);
-		$s3Secret = KeyValue::findValue(self::S3_SECRET);
-		$s3Bucket = KeyValue::findValue(self::S3_BUCKET);
+		$s3Key = KeyValue::findbyKey(self::S3_KEY);
+		$s3Secret = KeyValue::findbyKey(self::S3_SECRET);
+		$s3Bucket = KeyValue::findbyKey(self::S3_BUCKET);
 
+		// Instantiate the S3 client with your AWS credentials
 		$s3Client = S3Client::factory(array(
 			'key'    => $s3Key,
 			'secret' => $s3Secret,
-			'region' => KeyValue::findValue(self::S3_REGION)
+			'region' => KeyValue::findbyKey(self::S3_REGION)
 			));
 
 		$policyDocument = '{"expiration":"2100-01-01T00:00:00Z","conditions":[{"bucket": "' . $s3Bucket . '"},{"acl": "public-read"}, ["starts-with", "$key", ""], ["starts-with", "$Content-Type", ""], 
@@ -55,29 +52,51 @@ class S3Controller extends \mata\web\Controller {
 
 	public function actionUploadSuccessful() {
 
-		$s3Endpoint = KeyValue::findValue(self::S3_ENDPOINT);
-		$s3Bucket = KeyValue::findValue(self::S3_BUCKET);
+		$s3Endpoint = KeyValue::findByKey(self::S3_ENDPOINT);
+		$s3Bucket = KeyValue::findByKey(self::S3_BUCKET);
 
-		$imageURL = $s3Endpoint . "/" . $s3Bucket  . "/" . urlencode(\Yii::$app->getRequest()->post("key"));
+		$imageURL = $s3Endpoint .  $s3Bucket  . "/" . urlencode(\Yii::$app->getRequest()->post("key"));
 		$documentId = \Yii::$app->getRequest()->get("documentId");
 
-		if($media = Media::find()->where(["DocumentId" => $documentId])->one())
-			$media->delete();
+<<<<<<< Updated upstream
+		Media::find()->where(["DocumentId" => $documentId])->one()->delete();
 
-		$pattern = '/([a-zA-Z\\\]*)-([a-zA-Z0-9]*)(::)?([a-zA-Z0-9]*)?/';
-		preg_match($pattern, $documentId, $matches);	
+		$model = new Media() ;
+		$model->attributes = array(
+			"Name" => \Yii::$app->getRequest()->post("name"),
+			"DocumentId" => $documentId,
+			"URI" => $imageURL,
+			"Width" => 0,
+			"Height" => 0,
+			"MimeType" => "default"
+			);
 
-		if(!empty($matches) && empty($matches[2])) {
-			$pk = uniqid('tmp_');
-			if(!empty($matches[4]))
-				$pk .= "::" . $matches[4];
+		if ($model->save() == false)
+			throw new CHttpException(500, $model->getTopError());
+=======
+		// if($media = Media::find()->where(["For" => $documentId])->one())
+		// 	$media->delete();
 
-			$documentId = $matches[1] . "-" . $pk;
-		}
+		// $pattern = '/([a-zA-Z\\\]*)-([a-zA-Z0-9]*)(::)?([a-zA-Z0-9]*)?/';
+		// preg_match($pattern, $documentId, $matches);	
 
-		$mediaWidth = 0; 
-		$mediaHeight = 0;
-		$mimeType = "default";
+		// $model = Media::find()->forItem($documentId)->one();
+
+		// if ($model == null)
+		// 	$model = new Media();
+
+		// if(!empty($matches) && empty($matches[2])) {
+		// 	$pk = uniqid('tmp_');
+		// 	if(!empty($matches[4]))
+		// 		$pk .= "::" . $matches[4];
+
+		// 	$documentId = $matches[1] . "-" . $pk;
+		// 	$model->disableVersioning();
+		// }
+
+		// $mediaWidth = 0; 
+		// $mediaHeight = 0;
+		// $mimeType = "default";
 
 		$imageAttributes = getimagesize($imageURL);
 
@@ -92,28 +111,55 @@ class S3Controller extends \mata\web\Controller {
 			$mimeType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
 		}
 
-		$model = new Media() ;
-		$model->attributes = array(
-			"Name" => \Yii::$app->getRequest()->post("name"),
-			"DocumentId" => $documentId,
-			"URI" => $imageURL,
-			"Width" => $mediaWidth,
-			"Height" => $mediaHeight,
-			"MimeType" => $mimeType
-			);
 
-		if ($model->save() == false)
-			throw new \yii\web\HttpException(500, $model->getTopError());
+		// $linkedModel = new DocumentId($documentId);
+		// $linkedModel = $linkedModel->getModel();
+
+		// $model->attributes = array(
+		// 	"Name" => \Yii::$app->getRequest()->post("name"),
+		// 	"For" => $documentId,
+		// 	"URI" => $imageURL,
+		// 	"Width" => $mediaWidth,
+		// 	"Height" => $mediaHeight,
+		// 	"MimeType" => $mimeType
+		// 	);
+
+		// if ($model->save() == false)
+		// 	throw new \yii\web\HttpException(500, $model->getTopError());
+
+
+		// // align revisions with base model
+		// if ($linkedModel != null) {
+
+		// 	$linkedModelRevision = 1;
+
+		// 	$existingRevision = $model->getLatestRevision();
+
+		// 	if ($existingRevision)
+		// 		$linkedModelRevision = $existingRevision->Revision + 1;
+			
+		// 	$oldRevision = Revision::find()->where(["DocumentId" => $existingRevision->DocumentId, "Revision" => $linkedModelRevision])->one();
+
+		// 	if ($oldRevision)
+		// 		$oldRevision->delete();
+
+		// 	$revision = $model->getLatestRevision();
+		// 	$revision->Revision = $linkedModelRevision;
+
+		// 	if ($revision->save() == false)
+		// 		throw new \yii\web\HttpException(500, $revision->getTopError());
+		// }
+
 
 		$mediaResponse = [
-		'Id' => $model->Id,
-		'Name' => $model->Name,
-		'URI' => $model->URI,
-		'DocumentId' => $model->DocumentId->getId(),
-		'Width' => $model->Width,
-		'Height' => $model->Height,
-		'MimeType' => $model->MimeType,
-		'Extra' => $model->Extra,
+			// 'Id' => time(), // Id is required by fineUploader
+			'Name' => \Yii::$app->getRequest()->post("name"),
+			'URI' => $imageURL,
+			'DocumentId' => $documentId,
+			'Width' => $mediaWidth,
+			'Height' => $mediaHeight,
+			'MimeType' => $mimeType,
+			// 'Extra' => $model->Extra,
 		];
 		
 		$this->setResponseContentType("application/json");
@@ -121,18 +167,10 @@ class S3Controller extends \mata\web\Controller {
 	}
 
 	public function actionSetRandomFileName() {
+>>>>>>> Stashed changes
 
 		$this->setResponseContentType("application/json");
-
-		$name = \Yii::$app->getRequest()->post("name");
-
-		$pathInfo = pathinfo($name);
-		$extension = isset($pathInfo["extension"]) ? $pathInfo["extension"] : "";
-
-		$objectName = md5(time() . $name) . ".$extension";
-
-		$response = array('key' => $objectName);
-		echo json_encode($response);
+		echo Json::encode($model);
 	}
 
 }
