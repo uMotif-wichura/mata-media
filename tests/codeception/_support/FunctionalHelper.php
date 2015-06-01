55506946b43418be90abe3e6c7ffe32d25a5c9bc
Marcin Wiatr <marcin@qi-interactive.com>
2015-06-01 20:21:34 +0100

    added basic framework for tests
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- tests/codeception/_support/FunctionalHelper.php
+++ tests/codeception/_support/FunctionalHelper.php
@@ -1,11 +1,4 @@
 <?php
-
-/**
- * @link http://www.matacms.com/
- * @copyright Copyright (c) 2015 Qi Interactive Limited
- * @license http://www.matacms.com/license/
- */
-
 namespace Codeception\Module;
 
 // here you can define custom actions
